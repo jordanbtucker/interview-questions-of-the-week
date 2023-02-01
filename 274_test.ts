0fb85647ea1a9abf70d0241c78390d5d49cf0567
Jordan Tucker <jordanbtucker@gmail.com>
2023-02-01 10:41:25 -0600

    test(274): uncomment tests that were commented during debugging

--- 274_test.ts
+++ 274_test.ts
@@ -1,19 +1,19 @@
 import { assertEquals } from 'https://deno.land/std@0.175.0/testing/asserts.ts'
 import { combineStrings } from './274.ts'
 
-// Deno.test('274.1', () => {
-//   assertEquals(
-//     combineStrings(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 5),
-//     ['a b c', 'd e f', 'g'],
-//   )
-// })
-//
-// Deno.test('274.2', () => {
-//   assertEquals(
-//     combineStrings(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 12),
-//     ['a b c d e f', 'g'],
-//   )
-// })
+Deno.test('274.1', () => {
+  assertEquals(
+    combineStrings(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 5),
+    ['a b c', 'd e f', 'g'],
+  )
+})
+
+Deno.test('274.2', () => {
+  assertEquals(
+    combineStrings(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 12),
+    ['a b c d e f', 'g'],
+  )
+})
 
 Deno.test('274.3', () => {
   assertEquals(
